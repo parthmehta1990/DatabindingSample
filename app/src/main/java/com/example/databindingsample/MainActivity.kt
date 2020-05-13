3429d208ad61094cd7591302a38da95866f9f313
Parth Mehta <parth@id8labs.com>
2020-05-13 17:17:24 +0530

    Databinding sample example completed

--- app/src/main/java/com/example/databindingsample/MainActivity.kt
+++ app/src/main/java/com/example/databindingsample/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.databindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.databindingsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding:ActivityMainBinding
+
+    val user:User=User("Test User","Welcome")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.user=user
+
+        binding.button.setOnClickListener {
+
+            binding.apply {
+
+                txtmsg.text=editText.text.toString()
+                textView.visibility=View.INVISIBLE
+                invalidateAll()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/databindingsample/User.kt
@@ -0,0 +1,3 @@
+package com.example.databindingsample
+
+data class User(var name:String="",var surname:String="")
